5e7a101cb80278b3d22d1440f9d47fe495a67116
H. Boyd <harrisonboyd400@gmail.com>
2023-08-25 15:30:36 -0500

    Find COM port implicitly

--- Program.cs
+++ Program.cs
@@ -1,67 +1,85 @@
 ﻿using System.IO.Ports;
+using Windows.Devices.SerialCommunication;
 
-using (SerialPort comPort = new SerialPort("COM3", 9600)) // Baudrate is unknown
+ushort vid = 0x045E;
+ushort pid = 0x078F;
+
+string aqs = SerialDevice.GetDeviceSelectorFromUsbVidPid(vid, pid);
+
+var myDevices = Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(aqs, null).GetResults();
+
+
+if (myDevices.Count == 0)
 {
-    try
-    {
-        try {
-            comPort.Open();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Could not open COM3");
-            Console.WriteLine(ex.Message);
-            Console.WriteLine("Press any key to close...");
-            Console.ReadKey();
-            comPort.Close();
-        }
-        Console.WriteLine("Successfully connected to COM3!");
+    Console.WriteLine("Could not find any serial ports for the Katana v2");
+    return;
+}
 
-        Console.WriteLine("Press any key to send the first command...");
-        Console.ReadKey();
+
+using SerialDevice device = SerialDevice.FromIdAsync(myDevices[0].Id).GetResults();
+{
+    using (SerialPort comPort = new SerialPort(device.PortName, 9600)) // Baudrate is unknown
+    {
         try
         {
-            SendDataToPort(comPort, "5a 3a 08 2b 00 01 01 ff 00 80 00"); // Should set all leds to #008000
+            try
+            {
+                comPort.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not open " + device.PortName);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to close...");
+                Console.ReadKey();
+                comPort.Close();
+            }
+            Console.WriteLine("Successfully connected to " + device.PortName + "!");
+
+            Console.WriteLine("Press any key to send the command...");
+            Console.ReadKey();
+            try
+            {
+                SendDataToPort(comPort, "5a 3a 08 2b 00 01 01 ff 00 80 00"); // Sets all leds to #008000
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not send first command");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to close...");
+                Console.ReadKey();
+                comPort.Close();
+                return;
+            }
+
+            Console.WriteLine("\nClosing program...");
+
+            comPort.Close();
         }
         catch (Exception ex)
         {
-            Console.WriteLine("Could not send first command");
-            Console.WriteLine(ex.Message);
+            Console.WriteLine($"General Error: {ex.Message}");
             Console.WriteLine("Press any key to close...");
-            Console.ReadKey();
-            comPort.Close();
-            return;
         }
-        
-        Console.WriteLine("\nDid the RGB LEDs change? possible answers: (y/n)");
-        char response = Console.ReadKey().KeyChar;
-        Console.WriteLine("\nClosing program...");
-
-        comPort.Close();
     }
-    catch (Exception ex)
+
+    static void SendDataToPort(SerialPort port, string data)
     {
-        Console.WriteLine($"General Error: {ex.Message}");
-        Console.WriteLine("Press any key to close...");
+        byte[] bytesToSend = StringToByteArray(data);
+        port.Write(bytesToSend, 0, bytesToSend.Length);
     }
-}
 
-static void SendDataToPort(SerialPort port, string data)
-{
-byte[] bytesToSend = StringToByteArray(data);
-port.Write(bytesToSend, 0, bytesToSend.Length);
-}
+    static byte[] StringToByteArray(string hex)
+    {
+        string[] hexValues = hex.Split(' ');
+        byte[] bytes = new byte[hexValues.Length];
 
-static byte[] StringToByteArray(string hex)
-{
-string[] hexValues = hex.Split(' ');
-byte[] bytes = new byte[hexValues.Length];
+        for (int i = 0; i < hexValues.Length; i++)
+        {
+            bytes[i] = Convert.ToByte(hexValues[i], 16);
+        }
 
-for (int i = 0; i < hexValues.Length; i++)
-{
-    bytes[i] = Convert.ToByte(hexValues[i], 16);
-}
-
-return bytes;
+        return bytes;
+    }
 }
 
