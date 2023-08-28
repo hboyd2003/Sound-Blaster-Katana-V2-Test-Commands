f2582cfdb6a7d9ad9387930844d6c014bc5ceddd
H. Boyd <harrisonboyd400@gmail.com>
2023-08-28 15:43:08 -0500

    Switch to DataWriter for serial communication

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO.Ports;
 using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
 
 ushort vid = 0x041E;
 ushort pid = 0x3260;
@@ -15,60 +16,44 @@ if (myDevices.Count == 0)
     return;
 }
 
+SerialDevice device = Task.Run(() => SerialDevice.FromIdAsync(myDevices[0].Id).AsTask()).Result;
 
-var device = Task.Run(() => SerialDevice.FromIdAsync(myDevices[0].Id).AsTask()).Result;
+DataWriter dw = new DataWriter(device.OutputStream);
 
-
-using (SerialPort comPort = new SerialPort(device.PortName, 9600)) // Baudrate is unknown
+try
 {
+    Console.WriteLine("Successfully connected to " + device.PortName + "!");
+
+    Console.WriteLine("Press any key to send the command...");
+    Console.ReadKey();
     try
     {
-        try
-        {
-            comPort.Open();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Could not open " + device.PortName);
-            Console.WriteLine(ex.Message);
-            Console.WriteLine("Press any key to close...");
-            Console.ReadKey();
-            comPort.Close();
-            return;
-        }
-        Console.WriteLine("Successfully connected to " + device.PortName + "!");
+        SendDataToPort(dw, "5a 3a 20 2b 00 01 01 ff 00 00 ff ff 00 ff 00 ff ff 00 00 ff 00 80 ff ff 00 ff ff ff bf 00 8c ff ff ff ff"); // Sets the 7 segments to Red, Green, Blue, Orange, Purple, Ye
 
-        Console.WriteLine("Press any key to send the command...");
-        Console.ReadKey();
-        try
-        {
-            SendDataToPort(comPort, "5a 3a 20 2b 00 01 01 ff ff 00 00 ff 00 ff 00 ff 00 00 ff ff ff 80 00 ff ff ff 00 ff 8c 00 bf ff ff ff ff"); // Sets the 7 segments to Red, Green, Blue, Orange, Purple, Yellow, White
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Could not send first command");
-            Console.WriteLine(ex.Message);
-            Console.WriteLine("Press any key to close...");
-            Console.ReadKey();
-            comPort.Close();
-            return;
-        }
-
-        Console.WriteLine("\nClosing program...");
-
-        comPort.Close();
     }
     catch (Exception ex)
     {
-        Console.WriteLine($"General Error: {ex.Message}");
+        Console.WriteLine("Could not send first command");
+        Console.WriteLine(ex.Message);
         Console.WriteLine("Press any key to close...");
+        Console.ReadKey();
+        return;
     }
+
+    Console.WriteLine("\nClosing program...");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"General Error: {ex.Message}");
+    Console.WriteLine("Press any key to close...");
 }
 
-static void SendDataToPort(SerialPort port, string data)
+static void SendDataToPort(DataWriter deviceWriter, string data)
 {
     byte[] bytesToSend = StringToByteArray(data);
-    port.Write(bytesToSend, 0, bytesToSend.Length);
+    deviceWriter.WriteBytes(bytesToSend);
+    deviceWriter.StoreAsync();
+    deviceWriter.FlushAsync();
 }
 
 static byte[] StringToByteArray(string hex)
