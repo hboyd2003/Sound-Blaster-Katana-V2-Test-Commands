f13a29fcbb3534b8e939b4a94a90e0ff109114b8
H. Boyd <8950185+hboyd2003@users.noreply.github.com>
2023-10-22 17:14:56 -0500

    Rework for better testing ability

---
+++ KatanaV2Device.cs
@@ -0,0 +1,256 @@
+﻿using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+using ABI.Windows.UI.Input.Inking.Analysis;
+using System.Diagnostics;
+using Windows.Networking;
+using System.Reflection;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Windows.Devices.Usb;
+using System;
+
+namespace Sound_Blaster_Katana_V2_Test_Commands;
+internal partial class KatanaV2Device
+{
+
+    public string DeviceName
+    {
+        get;
+        private set;
+    } = "Katana V2";
+
+    public bool DeviceConnected
+    {
+        get;
+        private set;
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public string UUID
+    {
+        get;
+        private set;
+    }
+
+    public bool DeviceFound
+    {
+        get;
+        private set;
+    }
+
+
+ 
+    private const ushort Vid = 0x1B1C;
+    private const ushort Pid = 0x0C0B;
+
+    private Regex serialNumberRegex = new (@"(?<=\d{4}\\)[\w\d]+");
+    private SerialDevice? _device;
+    private DataWriter? _deviceWriter;
+    private int deleteme = 0;
+    private static readonly string DeviceSelector = SerialDevice.GetDeviceSelectorFromUsbVidPid(Vid, Pid);
+    private DeviceWatcher _deviceWatcher;
+    public string? DeviceId
+    {
+        get;
+        private set;
+    }
+
+    public KatanaV2Device()
+    {
+        Console.WriteLine("Starting Katanav2 class");
+        _deviceWatcher = DeviceInformation.CreateWatcher(DeviceSelector);
+        _deviceWatcher.Added += DeviceAddedEvent;
+        _deviceWatcher.Removed += DeviceRemovedEvent;
+        _deviceWatcher.Start();
+        Console.WriteLine("Started search for KatanaV2");
+    }
+
+    // TODO: Proper async for KatanaV2's SendCommand
+    public async Task<bool> SendCommand(byte[] command)
+    {
+        if (_deviceWriter == null)
+        {
+            return false;
+        } 
+
+        //TODO: Check if command sent successfully
+        if (deleteme < 5)
+        {
+            deleteme++;
+            Console.WriteLine("Sending command");
+        }
+        _deviceWriter.WriteBytes(command);
+        try
+        {
+            _deviceWriter.StoreAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to send command");
+            return false;
+        }
+        if (deleteme < 5)
+        {
+            deleteme++;
+            Console.WriteLine("Sent command");
+        }
+        return true;
+    }
+
+    // TODO: Proper async for KatanaV2's DeviceAddedEvent
+    private async void DeviceAddedEvent(DeviceWatcher sender, DeviceInformation deviceInfo)
+    {
+        Console.WriteLine("Katana V2 Found!");
+        // We don't care what devices already exist until we want to connect
+        if (DeviceConnected || DeviceFound)
+        {
+            Console.WriteLine("New device found while there is already a device connected/found");
+            return;
+        }
+        DeviceFound = true;
+        DeviceId = deviceInfo.Id;
+        DeviceName = deviceInfo.Name;
+
+        UUID = serialNumberRegex.Match(deviceInfo.Id).Groups[0].Value;
+
+    }
+
+    // TODO: Proper async for KatanaV2's DeviceRemovedEvent
+    private async void DeviceRemovedEvent(DeviceWatcher sender, DeviceInformationUpdate deviceInfo)
+    {
+        if (deviceInfo.Id != DeviceId)
+        {
+            return;
+        }
+
+        if (DeviceFound)
+        {
+            DeviceId = null;
+            DeviceFound = false;
+        }
+
+        if (DeviceConnected)
+        {
+            DisconnectFromDevice();
+        }
+    }
+
+    // TODO: Proper async for KatanaV2's ErrorReceivedEvent
+    private async void ErrorReceivedEvent(SerialDevice sender, ErrorReceivedEventArgs eventArgs)
+    { 
+        DisconnectFromDevice();
+    }
+
+    public async Task<bool> UnlockDevice()
+    {
+        if (!DeviceFound || DeviceConnected || DeviceId == null)
+        {
+            Console.WriteLine("Device unlock was called when it should not have been.");
+            return false;
+        }
+
+
+        var firmwareUtilityProcess = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = Path.Combine(AppContext.BaseDirectory, "cudsp600_firmware_utility.exe"),
+                Arguments = $"auto ver /dv{Vid:X} /dp{Pid:X}", // Just gets the version so that it will unlock the device for us but not mess anything up.
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+        };
+        try
+        {
+            firmwareUtilityProcess.Start();
+        }
+        catch (Win32Exception ex)
+        {
+            if (ex.NativeErrorCode == 2) // File not found error code.
+            {
+                Console.WriteLine("Could not find cudsp600_firmware_utility.exe");
+            }
+            Console.WriteLine("Failed to run cudsp600_firmware_utility.exe");
+            return false;
+        }
+
+        // Check if unlock was successful
+        using var output = firmwareUtilityProcess.StandardOutput;
+        {
+            await firmwareUtilityProcess.WaitForExitAsync();
+            var processOutput = await output.ReadToEndAsync();
+            if (processOutput.Contains("unlock_comms [0]")) // Due to the programs poor logging there may be other random stuff before/after
+            {
+                return true;
+            }
+            Console.WriteLine("Failed to unlock device:\n\nOutput of cudsp600_firmware_utility.exe:\n" + processOutput);
+            return false;
+
+        }
+        
+    }
+
+    // TODO: Proper async for KatanaV2's ConnectToDevice
+    public async Task<bool> ConnectToDevice()
+    {
+        if (DeviceId == null || !DeviceFound || DeviceConnected)
+        {
+            return false;
+        }
+
+        if (!await UnlockDevice())
+        {
+            return false;
+        }
+        _device = await SerialDevice.FromIdAsync(DeviceId);
+        Console.WriteLine("Got serial port object");
+        
+
+        _deviceWriter = new DataWriter(_device.OutputStream);
+        Console.WriteLine("Opened serial port");
+
+        //TODO: Check if device was actually connected.
+        DeviceConnected = true;
+
+        Console.WriteLine("Turning on LEDs");
+
+        // Turn on LEDs (if they are off)
+        await SendCommand(new byte[] { 0x5a, 0x3a, 0x02, 0x25, 0x01 });
+        SendCommand(new byte[] { 0x5a, 0x3a, 0x02, 0x26, 0x01 });
+        Console.WriteLine("Finished Connecting to device.");
+
+        //var errorReceivedEventHandler = new Windows.Foundation.TypedEventHandler<SerialDevice, ErrorReceivedEventArgs>(this.ErrorReceivedEvent);
+        //_device.ErrorReceived += errorReceivedEventHandler;
+
+        return true;
+    }
+
+    // TODO: Proper async for KatanaV2's DisconnectFromDevice
+    public async Task<bool> DisconnectFromDevice()
+    {
+        try
+        {
+            if (DeviceConnected)
+            {
+                _device?.Dispose();
+                DeviceConnected = false;
+                return true;
+            }
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+        return false;
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,109 +1,28 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO.Ports;
-using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using Windows.Devices.Enumeration;
 using Windows.Devices.SerialCommunication;
 using Windows.Storage.Streams;
-using Windows.Devices.Custom;
+using Sound_Blaster_Katana_V2_Test_Commands;
 
-ushort vid = 0x041E;
-ushort pid = 0x3278;
-
-string aqs = SerialDevice.GetDeviceSelectorFromUsbVidPid(vid, pid);
-
-var myDevices = Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(aqs, null).GetResults();
-
-if (myDevices.Count == 0)
+KatanaV2Device katanaV2Device = new KatanaV2Device();
+for (int i = 0; katanaV2Device.DeviceFound == false && i <=5; i++)
 {
-    Console.WriteLine("Could not find any serial ports for the Katana v2");
+    Thread.Sleep(1000);
+}
+
+if (katanaV2Device.DeviceFound == false)
+{
+    Console.WriteLine("Device not found after five seconds");
     return;
 }
 
-SerialDevice device;
-try
-{
-    device = Task.Run(() => SerialDevice.FromIdAsync(myDevices[0].Id).AsTask()).Result;
-} catch (Exception e)
-{
-    Console.WriteLine("Could not get serial port from device!");
-    Console.WriteLine($"{e.Message}");
-    return;
+katanaV2Device.ConnectToDevice();
 
-}
-
-
-try
-{
-    SerialPort port = new SerialPort(device.PortName);
-    Console.WriteLine("Successfully found device at " + device.PortName + "!");
-    Console.WriteLine("Unlocking Device!");
-    var proc = new Process
-    {
-        StartInfo = new ProcessStartInfo
-        {
-            FileName = @"cudsp600_firmware_utility.exe",
-            Arguments = $"auto ver /dv{vid:X} /dp{pid:X}", // Just gets the version so that it will unlock the device for us but not mess anything up.
-            UseShellExecute = false,
-            RedirectStandardOutput = true,
-            CreateNoWindow = true
-        }
-    };
-    proc.Start();
-    proc.WaitForExit();
-
-    Console.WriteLine("Unlocked Device (hopefully)");
-
-    device.Dispose();
-
-    Console.WriteLine("Press any key to send the command...");
-    Console.ReadKey();
-    try
-    {
-        //SendDataToPort(dw, "77 68 6f 61 72 65 79 6f 75 1e 04 60 32 e8 a0 1a 17 5c 93 61 a0 bc 47 2c d5 bc c7 3c 1b 5c b3 e0 56 5c ab c4 b7 5c b3 c0 56 5c b3 e0 56 0d 0a");
-        //SendDataToPort(dw, "77 68 6f 61 72 65 79 6f 75 2e 4d 79 41 70 70 38 0d 0a");
-        Console.WriteLine("Connecting to device " + device.PortName + "!");
-        port.Open();
-        Console.WriteLine("Connected to device " + device.PortName + "!");
-        port.ReadTimeout = 1000;
-        port.WriteTimeout = 1000;
-
-        byte[] command = new byte[] { 0x5a, 0x03, 0x00};
-
-
-        command = new byte[]
-        {
-            0x53, 0x57, 0x5f, 0x4d, 0x4f, 0x44, 0x45, 0x31,
-            0x0d, 0x0a
-        };
-        port.Write(command, 0, command.Length);
-
-        // Turn on LEDs (if they are off)
-        command = new byte[] { 0x5a, 0x3a, 0x02, 0x25, 0x01 };
-        port.Write(command, 0, command.Length);
-        command = new byte[] { 0x5a, 0x3a, 0x02, 0x26, 0x01 };
-        port.Write(command, 0, command.Length);
-        command = new byte[] {
-            0x5a, 0x3a, 0x20, 0x2b, 0x00, 0x01, 0x01, 0xff, 0x00, 0x00, 0xff, 0xff, 0x00, 0xff, 0x00, 0xff, 0xff, 0x00, 0x00, 0xff, 0x00, 0x80, 0xff, 0xff, 0x00, 0xff, 0xff, 0xff, 0xbf, 0x00, 0x8c, 0xff, 0xff, 0xff, 0xff
-        }; // Sets the 7 segments to Red, Green, Blue, Orange, Purple, Yellow, White
-
-        port.Write(command, 0, command.Length);
-        port.Close();
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine("Could not send first command");
-        Console.WriteLine(ex.Message);
-        Console.WriteLine("Press any key to close...");
-        Console.ReadKey();
-        return;
-    }
-
-    Console.WriteLine("\nClosing program...");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"General Error: {ex.Message}");
-    Console.WriteLine("Press any key to close...");
-}
-
-
-
+Console.WriteLine("Device ID:" + katanaV2Device.DeviceId);
+Console.WriteLine("Device Name: " + katanaV2Device.DeviceName);
+Console.WriteLine("Device UUID: " + katanaV2Device.UUID);
